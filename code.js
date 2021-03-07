39ce6d3d031488ecd110b2ec3cf4a4788a973efe
GridTrekkor <jwo@umn.edu>
2021-03-07 11:38:12 -0600

    Format code

--- code.js
+++ code.js
@@ -3,22 +3,25 @@ var num2;
 var answer;
 
 function getProblem() {
-   num1 = randomNumber(2, 9);
-   num2 = randomNumber(2, 9);
-  var prob = num1.toString() + " x " + num2.toString();
-  return prob;
+	num1 = randomNumber(2, 9);
+	num2 = randomNumber(2, 9);
+	var prob = num1.toString() + " x " + num2.toString();
+	return prob;
 }
-onEvent("button1", "click", function( ) {
+
+onEvent("button1", "click", function () {
 	setText("label1", getProblem());
 });
-onEvent("input1", "input", function() {
+
+onEvent("input1", "input", function () {
 	answer = getText("input1");
 });
-onEvent("button2", "click", function( ) {
+
+onEvent("button2", "click", function () {
 	answer = parseInt(answer);
 	if (num1 * num2 === answer) {
-	  setText("label2", "Correct!");
+		setText("label2", "Correct!");
 	} else {
-	  setText("label2", "Sorry, try again!");
+		setText("label2", "Sorry, try again!");
 	}
 });
